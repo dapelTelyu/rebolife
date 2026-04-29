7d4dbd2800684d060ec895f01402364336d3032e
dapelTelyu <daffanovel@student.telkomuniversity.ac.id>
2026-04-29 11:22:16 +0700

    simple aja

---
+++ copy.ps1
@@ -0,0 +1,3 @@
+New-Item -ItemType Directory -Force -Path ".\images"
+Copy-Item -Path "C:\Users\daffa\.gemini\antigravity\brain\77574222-58c4-44d0-83e1-2b1a8ab1b1fa\bottle_robot_craft_1777435946060.png" -Destination ".\images\bottle_robot.png"
+Copy-Item -Path "C:\Users\daffa\.gemini\antigravity\brain\77574222-58c4-44d0-83e1-2b1a8ab1b1fa\kids_exhibition_hero_1777435966334.png" -Destination ".\images\kids_hero.png"
